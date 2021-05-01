43cd5f9b53f4aeadce7956771a4543f2f1e23a50
Andrew Hejl <andrew.hejl@outlook.com>
2021-05-01 14:11:55 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinearRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point Andre = new Point("Andre", 165, 66);
+            Point Aaron = new Point("Aaron", 185, 71);
+            Point James = new Point("James", 190, 70);
+            Point Charles = new Point("Charles", 210, 72);
+            Point Jacon = new Point("Jacon", 180, 72);
+            Point Kindra = new Point("Kindra", 150, 67);
+            Point Malachi = new Point("Malachi", 180, 68);
+
+
+            Point.doFormula();
+
+        }
+    }
+}
---
+++ Repo.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinearRegression
+{
+    class Point
+    {
+        public static List<Point> pointList = new List<Point>();
+
+        public string Name { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public static double sumX()
+        {
+            double sum = 0.0;
+            foreach(Point item in pointList)
+            {
+                sum += item.X;
+            }
+            return sum;
+        }
+        public static double sumXSqr()
+        {
+            double sum = 0.0;
+            foreach (Point item in pointList)
+            {
+                sum += (item.X * item.X);
+            }
+            return sum;
+        }
+
+        public static double sumY()
+        {
+            double sum = 0.0;
+            foreach (Point item in pointList)
+            {
+                sum += item.Y;
+            }
+            return sum;
+        }
+        public static double sumXY()
+        {
+            double sum = 0.0;
+            foreach (Point item in pointList)
+            {
+                sum += (item.X * item.Y);
+            }
+            return sum;
+        }
+
+        public static int getN()
+        {
+            int N = pointList.Count;
+            return N;
+        }
+
+        public static double getAlpha()
+        {
+            double alpha = 0.0;
+            double sX = sumX();
+            double sXS = sumXSqr();
+            double sY = sumY();
+            double sXY = sumXY();
+            double N = getN();
+
+            alpha = (sY * sXS - sX * sXY) / (N * sXS - Math.Pow(sX,2));
+
+            return alpha;
+        }
+
+        public static double getBeta()
+        {
+            double beta = 0.0;
+            beta = (getN() * sumXY() - sumX() * sumY()) 
+                 / (getN() * sumXSqr() - (Math.Pow(sumX(), 2)));
+            return beta;
+        }
+
+        public static void getFormula()
+        {
+            double A = getAlpha();
+            double B = getBeta();
+            Console.WriteLine($"Y = {A} + {B} * X");
+        }
+
+        public static void doFormula()
+        {
+            Console.WriteLine("The forumula for the inputed data is:");
+            getFormula();
+            Console.WriteLine("Please enter a value for X as a predictor:");
+            double pred = double.Parse(Console.ReadLine());
+            double A = getAlpha();
+            double B = getBeta();
+            double result = pred * B + A;
+            result = Math.Round(result, 2);
+            Console.WriteLine($"For X of {pred} Y is {result}.");
+        }
+
+        public Point() 
+        {
+            this.Name = "Unknown";
+            this.X = 0.0;
+            this.Y = 0.0;
+            pointList.Add(this);
+        }
+        public Point(string inName = "Unknown", double inX = 0, double inY = 0) 
+        {
+            this.Name = inName;
+            this.X = inX;
+            this.Y = inY;
+            pointList.Add(this);
+        }
+        public Point(string inName = "Unknown", int inX = 0, int inY = 0) 
+        {
+            this.Name = inName;
+            this.X = inX;
+            this.Y = inY;
+            pointList.Add(this);
+        }
+    }
+}
